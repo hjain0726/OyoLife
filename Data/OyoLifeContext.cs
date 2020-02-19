4e95ced99fc2ee421490e275ccd3709e21a327c1
hjain0726 <hjain0726@gmail.com>
2020-02-19 22:45:30 +0530

    initial setup

---
+++ Data/OyoLifeContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using OyoLife.Models;
+
+namespace OyoLife.Data
+{
+    public class OyoLifeContext : DbContext
+    {
+        public OyoLifeContext (DbContextOptions<OyoLifeContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<OyoLife.Models.User> User { get; set; }
+    }
+}
---
+++ Models/ResponseMsg.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OyoLife.Models
+{
+    public class ResponseMsg
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public string Token { get; set; }
+        public User user { get; set; }
+    }
+}
